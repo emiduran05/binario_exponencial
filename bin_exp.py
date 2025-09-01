495a06b2eb1160835f74a8b96b8bf354dabd07dd
emiduran05 <emiduranf05@gmail.com>
2025-08-31 20:56:04 -0600

    log img

--- bin_exp.py
+++ bin_exp.py
@@ -18,5 +18,17 @@ def bin_exp(base, exponente, resultado ):
     
     
     
-print(bin_exp(2,12,1)) #base, exponente, y el resultado siempre se inicializa en 1
-print(pow(2,12)) #Da el mismo resultado usando la funcion pow()
+print("Funcion con recursividad: ", bin_exp(2,12,1), ", Función con Pow: ", pow(2,12)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(2,10,1), ", Función con Pow: ", pow(2,10)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(2,2,1), ", Función con Pow: ", pow(2,2)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(3,10,1), ", Función con Pow: ", pow(3,10)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(4,15,1), ", Función con Pow: ", pow(4,15)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(5,3,1), ", Función con Pow: ", pow(5,3)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(3,9,1), ", Función con Pow: ", pow(3,9)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(6,10,1), ", Función con Pow: ", pow(6,10)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(8,18,1), ", Función con Pow: ", pow(8,18)) #base, exponente, y el resultado siempre se inicializa en 1
+print("Funcion con recursividad: ", bin_exp(4,6,1), ", Función con Pow: ", pow(4,6)) #base, exponente, y el resultado siempre se inicializa en 1
+
+
+
+
